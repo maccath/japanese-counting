72adbee1cbe4d960243cd7f0a27c0a57b1d4678b
Katy Ereira <katye@deeson.co.uk>
2017-02-23 23:07:49 +0000

    Display a random number from 1-999 on the homepage.

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,7 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
+$app->get('/', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
