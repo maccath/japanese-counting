f60eabacc23cbcb3b3cf9e1a879088e65c943c1e
Katy Ereira <katye@deeson.co.uk>
2017-02-23 20:49:25 +0000

    Convert integer to Japanese kanji for numbers 1 to 10.

--- src/JapaneseNumeral.php
+++ src/JapaneseNumeral.php
@@ -7,11 +7,33 @@ class JapaneseNumeral
     /** @var int */
     private $number;
 
+    /** @var array */
+    const DIGITS = [
+        1 => '一',
+        2 => '二',
+        3 => '三',
+        4 => '四',
+        5 => '五',
+        6 => '六',
+        7 => '七',
+        8 => '八',
+        9 => '九',
+        10 => '十',
+    ];
+
     /**
      * @param int $number
      */
     public function __construct(int $number)
     {
         $this->number = $number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKanji(): string
+    {
+        return self::DIGITS[$this->number];
     }
 }
--- tests/Unit/JapaneseNumeralTest.php
+++ tests/Unit/JapaneseNumeralTest.php
@@ -18,4 +18,32 @@ class JapaneseNumeralTest extends \PHPUnit_Framework_TestCase
     {
         new JapaneseNumeral(1.5);
     }
+
+    /**
+     * @dataProvider numeralsKanjiProvider
+     * @param int $int
+     * @param string $kanji
+     */
+    public function testJapaneseNumeralCreatesKanji(int $int, string $kanji)
+    {
+        $japaneseNumeral = new JapaneseNumeral($int);
+
+        $this->assertEquals($kanji, $japaneseNumeral->getKanji());
+    }
+
+    public function numeralsKanjiProvider()
+    {
+        return [
+          [1, '一'],
+          [2, '二'],
+          [3, '三'],
+          [4, '四'],
+          [5, '五'],
+          [6, '六'],
+          [7, '七'],
+          [8, '八'],
+          [9, '九'],
+          [10, '十'],
+        ];
+    }
 }
