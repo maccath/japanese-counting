abc55446f1ae1f494aa4b28b35f3f360f7c8cb6d
Katy Ereira <katye@deeson.co.uk>
2017-02-26 18:57:18 +0000

    Integrate Travis CI.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_0e2c24ae3379_key -iv $encrypted_0e2c24ae3379_iv -in deploy-key.enc -out ~\/.ssh/deploy-key -d
+openssl aes-256-cbc -K $encrypted_0e2c24ae3379_key -iv $encrypted_0e2c24ae3379_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
